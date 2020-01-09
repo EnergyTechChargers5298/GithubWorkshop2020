2b0ab303195ad55158eb1ae0b17c08de4713d594
Jessica <jgarcia@etech-nyc.org>
2020-01-09 14:13:50 -0600

    Hello World

--- ExampleHello.java
+++ ExampleHello.java
@@ -1,5 +1,8 @@
-public class ExampleHello {
+// Template for your file
+public class ExampleHello { 
+    // Create your entry point
     public static void main(String[] args) {
+    // Print some code!
         System.out.println("Hello World");
         System.out.println("Welcome to our workshop!");
         System.out.println("Let's go coders!");
