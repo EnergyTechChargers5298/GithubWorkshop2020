be62c278530550f00f6e37d594f20ffd287f02aa
Jessica <jgarcia@etech-nyc.org>
2020-01-08 21:56:10 -0600

    An absurd greeting

--- ExampleHello.java
+++ ExampleHello.java
@@ -2,5 +2,6 @@ public class ExampleHello {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Jessica Garcia");
+        System.out.println("It is Wednesday my dudes.");
     }
 }
