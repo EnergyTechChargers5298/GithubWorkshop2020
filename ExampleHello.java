7a9ab59aa24a21dbc4d7a463831441e91cfa2dd7
Jessica <jgarcia@etech-nyc.org>
2020-01-09 13:59:34 -0600

    Coder Cheer

--- ExampleHello.java
+++ ExampleHello.java
@@ -1,7 +1,7 @@
 public class ExampleHello {
     public static void main(String[] args) {
-        System.out.println("It'sJessica Garcia");
-        System.out.println("and it's Wednesday my dudes.");
-        System.out.println("REEEEEEE");
+        System.out.println("Hello World");
+        System.out.println("Welcome to our workshop!");
+        System.out.println("Let's go coders!");
     }
 }
