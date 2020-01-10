2ede38a89482f9daf2ba2e9ab57e68e8f2ea56e5
pepetheDimomdFrg <oelsayed@etech-nyc.org>
2020-01-10 17:35:26 -0500

    i made a multiply method

---
+++ Omar.java
@@ -0,0 +1,15 @@
+public class Omar{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        System.out.println("MY NAME IS OMAR ELSAYED");
+        multiply(10, 3);
+        multiply(8, 9);
+    }
+
+    // Method that multiply 2 numbers
+    public static void multiply(int num1, int num2)  {
+         int Value = num1 * num2;
+         System.out.println(Value);
+    }
+    
+}
