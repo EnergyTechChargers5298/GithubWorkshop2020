baa34fcfb12fcc447995d851d4f8173c2bd97669
razsta4ax <nbaletin@etech-nyc.org>
2020-01-10 17:34:54 -0500

    I made a multiplying method

---
+++ NikolaB.java
@@ -0,0 +1,16 @@
+public class NikolaB {
+    public static void main(String[] args) {
+       System.out.println("Hello World"); 
+       System.out.println("My name is Nikola Baletin");
+       multiply(10, 3);
+       multiply(8, 9);
+       multiply(1348, 2506);
+    }
+
+    // Method that multiplies two numbers
+    public static void multiply(int num1, int num2) {
+        int value = num1 * num2;
+        System.out.println(value);
+
+    }
+}
