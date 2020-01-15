596f02e3b85fb0a48f6e7993f37d6285214a1127
Gloryhunter305 <moporto@etech-nyc.org>
2020-01-15 16:59:19 -0500

    Add my code please

--- ExampleHello.java
+++ ExampleHello.java
@@ -1,10 +1,23 @@
 // Template for your file
 public class ExampleHello { 
+    
     // Create your entry point
     public static void main(String[] args) {
     // Print some code!
         System.out.println("Hello World");
-        System.out.println("Welcome to our workshop!");
-        System.out.println("Let's go coders!");
+        
+        int numberOne = 20;
+        int numberTwo = 10;
+
+        int remainder = numberOne % numberTwo;
+
+        System.out.println(remainder);
+
+        multiply(20, 12);
+    }
+
+    public static void multiply (int num1, int num2) {
+        int value = num1 * num2;
+        System.out.println(value);
     }
 }
