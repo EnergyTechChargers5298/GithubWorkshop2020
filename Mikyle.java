a246c3f944bfd1b2cf4c462275cf574b58986e5c
Gloryhunter305 <moporto@etech-nyc.org>
2020-01-15 17:02:26 -0500

    Add my code

---
+++ Mikyle.java
@@ -0,0 +1,23 @@
+// Template for your file
+public class Mikyle { 
+    
+    // Create your entry point
+    public static void main(String[] args) {
+    // Print some code!
+        System.out.println("Hello World");
+        
+        int numberOne = 20;
+        int numberTwo = 10;
+
+        int remainder = numberOne % numberTwo;
+
+        System.out.println(remainder);
+
+        multiply(20, 12);
+    }
+
+    public static void multiply (int num1, int num2) {
+        int value = num1 * num2;
+        System.out.println(value);
+    }
+}
