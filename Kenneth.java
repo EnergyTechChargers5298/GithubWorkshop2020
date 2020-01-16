e8a81e338250093a847178243ed9d2a887c5379f
botkii <kennw902@gmail.com>
2020-01-15 21:14:59 -0500

    hi i did multiply

---
+++ Kenneth.java
@@ -0,0 +1,12 @@
+public class Kenneth {
+    public static void main (String[] args) {
+        System.out.println("Hello world.");
+        int x = multiply(3, 10);
+        System.out.println(x);
+    }
+    public static int multiply (int a, int b) {
+        int c = a * b;
+        return c;
+    }
+
+}
