fb9c0d0343c0cdf07fa4b7abbf32d07e3c1f1eb1
BotRamenRobotics <redstonegamer360@gmail.com>
2020-01-16 16:09:41 -0500

    Kenneth did something

---
+++ Raymond.java
@@ -0,0 +1,11 @@
+public class Raymond {
+    public static void main(String[] args) {
+        System.out.println("Hello world?");
+        int n = multiply(307, 901);
+        System.out.println(n);
+    }
+    public static int multiply (int k, int s)  {
+        int l = k * s;
+        return l;
+    }
+} 
